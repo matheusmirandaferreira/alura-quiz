e2821d00cf2454e0e06611c7985b87f93f7ceb48
Matheus de Miranda Ferreira <ohhhmiranha@gmail.com>
2021-02-01 17:32:56 -0300

    Alura Quiz - Imersão ReactJS e NextJS - v2.

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import QuizScreen from '../../src/Screens/Quiz';
+import QuizScreen from '../../src/screens/Quiz';
 import db from '../../db.json';
 
 function QuizDaGaleraPage() {
